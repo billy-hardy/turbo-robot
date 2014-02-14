cb0e1e3fa85a2fe9ed38af0e6eca8705baf538cb
Billy Hardy <billy92@gmail.com>
2014-02-14 17:48:56 -0500

    removed some old files

--- sa.py
+++ sa.py
@@ -2,9 +2,11 @@ import sys
 sys.dont_write_bytecode=True
 from models import *
 from stats import run
+from do import *
 
-
-def sa(m=kursawe, era=200, eps=0.2, p=0.33, initial=None):
+def sa(m=kursawe, runs=50,
+			 era=30, kmax=1000,
+			 eps=0.1, p=0.33, cohen=0.2):
 	def fiddle(model, ind):
 		ret = []
 		for i,d in enumerate(model.decs):
@@ -14,32 +16,37 @@ def sa(m=kursawe, era=200, eps=0.2, p=0.33, initial=None):
 				ret.append(ind[i])
 		return ret
 	def maybe(old,new,t):
-		x = e**((old - new)*1.0/t)
+		x = math.e**((old - new)*1.0/t)
 		y = rand()
 		return x < y
-	model = m()
-	c = run()
-	ind = ind_b = model.get_ind()
-	dep = model.get_dep(ind)
-	e = e_b = model.score(dep)
-	c.add(ind+[e], ind_b+[e_b])
-	stagger = 1.0
-	k = 0.0
-	for _ in xrange(era):
-		k += 1
-		ind_n = fiddle(model,ind[:])
-		dep_n = model.get_dep(ind_n[:])
-		e_n = model.score(dep_n)
-		if e_n > e_b:
-			ind_b = ind_n[:]
-			e_b = e_n
-			c.add(ind+[e], ind_b+[e_b])
-		if e_n > e:
-			ind = ind_n[:]
-			e = e_n
-			c.add(ind+[e], ind_b+[e_b])
-		elif maybe(e, e_n, (k/era)**stagger):
-			ind = ind_n[:]
-			e = e_n
-			c.add(ind+[e], ind_b+[e_b])
-	return c
+	outer = do(range(runs))
+	for run, outer in outer.loop():
+		model = m()
+		model.baseline()
+		ind = ind_b = model.get_ind()
+		dep = model.get_dep(ind)
+		e = e_b = model.score(dep)
+		inner = do(range(kmax),
+							 eps=eps, halt_on="best",
+							 era=era, also=outer, cohen=cohen)
+		k = 0.0
+		for _, inner in inner.loop():
+			k+=1
+			ind_n = fiddle(model,ind[:])
+			dep_n = model.get_dep(ind_n[:])
+			e_n = model.score(dep_n)
+			if e_n > e_b:
+				ind_b = ind_n[:]
+				e_b = e_n
+			if e_n > e:
+				ind = ind_n[:]
+				e = e_n
+			elif maybe(e, e_n, k/kmax):
+				ind = ind_n[:]
+				e = e_n
+			inner.seen(k, best=e_b, every=e)
+	done(outer, 0, 1,
+			 key=lambda x: '%2d'%x,
+			 value = lambda x: '%4.2f'%x)
+			
+sa(m=fonseca,runs=50, eps=0.01, cohen=0.2)
